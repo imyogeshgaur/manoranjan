3bb09854fc119b13c38a9cbc22699aa81b31cc4d
imyogeshgaur <yogeshgaur1995@gmail.com>
2024-06-10 22:34:04 +0530

    Show all video logic completed

--- frontend/src/Home/AllVideo.tsx
+++ frontend/src/Home/AllVideo.tsx
@@ -1,9 +1,68 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router";
+import callAPIOnButtonClick from "../api/CallAPI";
+import VideoCard from "../Components/Card";
+import NavBar from "../Components/NavBar";
 
 const AllVideo = () => {
-  return (
-    <div>AllVideo</div>
-  )
-}
+  const [videosState, setVideosState] = useState({
+    videos: [],
+    message: "",
+  });
 
-export default AllVideo
+  const token = localStorage.getItem("token");
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (token === null) navigate("/");
+    const headers = {
+      authorization: token,
+    };
+    callAPIOnButtonClick(
+      "GET",
+      "http://localhost:4000/api/video/allVideos",
+      {},
+      headers
+    )
+      .then((responseFromBackend: any) => {
+        const { statusFromBackend, dataFromBackend }: any = responseFromBackend;
+        if (statusFromBackend === 200)
+          setVideosState({ message: "", videos: dataFromBackend.videos });
+        else setVideosState({ message: dataFromBackend.message, videos: [] });
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  return (
+    <>
+      <NavBar />
+      <div style={styles.cardContainer}>
+        {videosState.videos.length === 0 ? (
+          <h1>{videosState.message}</h1>
+        ) : (
+          videosState.videos.map((video: any, index: any) => (
+            <VideoCard
+              key={index}
+              videoId={video.videoId}
+              title={video.titleOfVideo}
+              description={video.descriptionOfVideo}
+              url={video.urlOfVideo}
+            />
+          ))
+        )}
+      </div>
+    </>
+  );
+};
+
+const styles = {
+  header: {
+    margin: "20px 0",
+  },
+  cardContainer: {
+    display: "flex",
+    justifyContent: "space-around",
+  },
+};
+
+export default AllVideo;
