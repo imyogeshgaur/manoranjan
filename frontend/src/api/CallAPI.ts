676a8e5a1a44d8f3e0b3100467dbec91b938d99e
imyogeshgaur <yogeshgaur1995@gmail.com>
2024-06-10 22:31:37 +0530

    changed post and put method custom function logic for file input

--- frontend/src/api/CallAPI.ts
+++ frontend/src/api/CallAPI.ts
@@ -1,19 +1,41 @@
 import axios from "axios"
 
-const callAPIOnButtonClick = async (type: string, url: string, parameters?: object, headers?: object) => {
+const callAPIOnButtonClick = async (type: string, url: string, parameters?: object, headers?: object, formData?: any, isFileThere?: boolean) => {
     try {
         if (type == "GET") {
             const response = await axios.get(url, { headers });
             const data = await response.data;
             return { statusFromBackend: response.status, dataFromBackend: data };
         } else if (type == "POST") {
-            const response = await axios.post(url, parameters, { headers });
-            const data = await response.data;
-            return { statusFromBackend: response.status, dataFromBackend: data };
+            if (isFileThere) {
+                const response = await axios({
+                    method: "POST",
+                    baseURL: url,
+                    data: formData,
+                    headers
+                })
+                const data = await response.data;
+                return { statusFromBackend: response.status, dataFromBackend: data };
+            } else {
+                const response = await axios.post(url, parameters, { headers, data: formData });
+                const data = await response.data;
+                return { statusFromBackend: response.status, dataFromBackend: data };
+            }
         } else if (type == "PUT") {
-            const response = await axios.put(url, parameters, { headers });
-            const data = await response.data;
-            return { statusFromBackend: response.status, dataFromBackend: data };
+            if (isFileThere) {
+                const response = await axios({
+                    method: "PUT",
+                    baseURL: url,
+                    data: formData,
+                    headers
+                })
+                const data = await response.data;
+                return { statusFromBackend: response.status, dataFromBackend: data };
+            } else {
+                const response = await axios.put(url, parameters, { headers });
+                const data = await response.data;
+                return { statusFromBackend: response.status, dataFromBackend: data };
+            }
         } else {
             const response = await axios.delete(url, parameters);
             const data = await response.data;
