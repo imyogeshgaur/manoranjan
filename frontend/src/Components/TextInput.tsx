7019b52f9fa8f5a046e5236b622b23440138215d
imyogeshgaur <yogeshgaur1995@gmail.com>
2024-06-10 22:33:30 +0530

    Added some more input types

--- frontend/src/Components/TextInput.tsx
+++ frontend/src/Components/TextInput.tsx
@@ -1,16 +1,43 @@
-
-const TextInput = (props:any) => {
-  return (
-        <input 
-            type={props.type}
-            placeholder={props.placeholder}
-            className={props?.className}
-            style={props?.style}
-            value={props.value}
-            name={props?.name}
-            onChange={props.onChange}
+const TextInput = (props: any) => {
+  if(props.isTextBox){
+    return (
+      <textarea 
+        placeholder={props.placeholder}
+        className={props?.className}
+        style={props?.style}
+        value={props.value}
+        name={props?.name}
+        onChange={props.onChange}
+        rows={props.row}
+        cols={props.col}
+      />
+    )
+  }else{
+    if(props.type==='file'){
+      return (
+        <input
+          type={props.type}
+          title={props.placeholder}
+          style={{backgroundColor:"white"}}
+          name={props?.name}
+          readOnly={true}
+          onChange={props.onChange}
         />
-  )
-}
+      );
+    }else{
+      return (
+        <input
+          type={props.type}
+          placeholder={props.placeholder}
+          className={props?.className}
+          style={props?.style}
+          value={props.value}
+          name={props?.name}
+          onChange={props.onChange}
+        />
+      );
+    }
+  }
+};
 
-export default TextInput
+export default TextInput;
